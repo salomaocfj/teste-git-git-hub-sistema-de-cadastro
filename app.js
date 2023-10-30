28da4a51770266aabca333c3ddd90a822beffa2b
salomaocfj <salomaocfj@yahoo.com.br>
2023-10-30 19:02:22 -0300

    adições temporárias

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos.");
 console.log("A aplicação está se conectando ao banco de dados.");
+//Adicionado para ver como faz um restore de versão
+console.log("Isso aqui foi adicionado no intervalo pra depois ser removido.");
